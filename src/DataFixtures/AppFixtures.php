318f4542665bd0552a76508eb294fd5a7c1a6285
Boris Urosevic <borisurosevic1@gmail.com>
2022-01-05 23:29:36 +0100

    Punjenje baze test podacima

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,40 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\BlogPost;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $blogPost = new BlogPost();
+        $blogPost->setTitle('A first post!');
+        $blogPost->setPublished(new \DateTime('2018-07-01 12:00:00'));
+        $blogPost->setContent('Post text!');
+        $blogPost->setAuthor('Boris');
+        $blogPost->setSlug('a-first-post');
+
+        $manager->persist($blogPost);
+
+        $blogPost = new BlogPost();
+        $blogPost->setTitle('A second post!');
+        $blogPost->setPublished(new \DateTime('2018-07-05 06:14:40'));
+        $blogPost->setContent('Post text second!');
+        $blogPost->setAuthor('Bora');
+        $blogPost->setSlug('a-second-post');
+
+        $manager->persist($blogPost);
+
+        $blogPost = new BlogPost();
+        $blogPost->setTitle('A third post!');
+        $blogPost->setPublished(new \DateTime('2018-07-06 15:18:03'));
+        $blogPost->setContent('Post text third!');
+        $blogPost->setAuthor('Boris');
+        $blogPost->setSlug('a-third-post');
+
+        $manager->persist($blogPost);
 
         $manager->flush();
     }
