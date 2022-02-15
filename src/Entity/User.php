4a001995dc00af29abd10ace52e069428226b74c
Boris Urosevic <borisurosevic1@gmail.com>
2022-02-15 20:36:45 +0100

    Grupe pristupa poljima

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -7,12 +7,16 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\Collection;
 use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ApiResource(
  *      itemOperations={"get"},
- *      collectionOperations={}
+ *      collectionOperations={"post"},
+ *      normalizationContext={
+ *          "groups"={"read"}
+ *      }
  * )
  * @ORM\Entity(repositoryClass=UserRepository::class)
  */
@@ -22,11 +26,13 @@ class User implements UserInterface
      * @ORM\Id
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
+     * @Groups({"read"})
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"read"})
      */
     private $username;
 
@@ -37,6 +43,7 @@ class User implements UserInterface
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"read"})
      */
     private $name;
 
@@ -47,11 +54,13 @@ class User implements UserInterface
 
     /**
      * @ORM\OneToMany(targetEntity="App\Entity\BlogPost", mappedBy="author")
+     * @Groups({"read"})
      */
     private $posts;
 
     /**
      * @ORM\OneToMany(targetEntity="App\Entity\Comment", mappedBy="author")
+     * @Groups({"read"})
      */
     private $comments;
 
